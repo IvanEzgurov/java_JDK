14bb81914d94ce4d24ee5e74494542ad24956e32
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-12-14 23:32:31 +0300

    HW3_finish

---
+++ src/main/java/org/example/homework3/CompareArrays.java
@@ -0,0 +1,11 @@
+package org.example.homework3;
+
+public class CompareArrays <T> {
+    public static <T> boolean compareTwoArrays(T[] array1, T[] array2){
+        if(array1.length != array2.length) return false;
+        for(int i = 0; i < array1.length; i++){
+            if(!array1[i].equals(array2[i])) return false;
+        }
+        return true;
+    }
+}
